376b30f6c2fd2aed23e7b14f25a70728bd387eb0
selmi-karim <selmi.kerim@gmail.com>
2020-05-30 23:11:50 +0100

    feat: eslint & format

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
 const twoStrings = (sa1, sa2) => {
-    // Compare two strings to see how similar they are.
-    // Answer is returned as a value from 0 - 1
-    // 1 indicates a perfect similarity (100%) while 0 indicates no similarity (0%)
-    // The algorithm is an n-gram comparison of bigrams of characters in a string
+  // Compare two strings to see how similar they are.
+  // Answer is returned as a value from 0 - 1
+  // 1 indicates a perfect similarity (100%) while 0 indicates no similarity (0%)
+  // The algorithm is an n-gram comparison of bigrams of characters in a string
 
-    // Comparison should not check case or whitespace
-    let s1 = sa1.replace(/\s/g, "").toLowerCase();
-    let s2 = sa2.replace(/\s/g, "").toLowerCase();
+  // Comparison should not check case or whitespace
+  let s1 = sa1.replace(/\s/g, "").toLowerCase();
+  let s2 = sa2.replace(/\s/g, "").toLowerCase();
 
-    const similarity_num = 2 * intersect(pairs(s1), pairs(s2)).length;
-    const similarity_den = pairs(s1).length + pairs(s2).length;
-    return similarity_num / similarity_den;
+  const similarity_num = 2 * intersect(pairs(s1), pairs(s2)).length;
+  const similarity_den = pairs(s1).length + pairs(s2).length;
+  return similarity_num / similarity_den;
 };
 
 // private functions ---------------------------
 const intersect = (arr1, arr2) => {
-    let r = [], o = {}, l = arr2.length, i, v;
-    for (i = 0; i < l; i++) {
-        o[arr2[i]] = true;
+  let r = [],
+    o = {},
+    l = arr2.length,
+    i,
+    v;
+  for (i = 0; i < l; i++) {
+    o[arr2[i]] = true;
+  }
+  l = arr1.length;
+  for (i = 0; i < l; i++) {
+    v = arr1[i];
+    if (v in o) {
+      r.push(v);
     }
-    l = arr1.length;
-    for (i = 0; i < l; i++) {
-        v = arr1[i];
-        if (v in o) {
-            r.push(v);
-        }
-    }
-    return r;
-}
+  }
+  return r;
+};
 
 // private functions ---------------------------
 const pairs = (s) => {
-    // Get an array of all pairs of adjacent letters in a string
-    let pairs = [];
-    for (let i = 0; i < s.length - 1; i++) {
-        pairs[i] = s.slice(i, i + 2);
-    }
-    return pairs;
-}
+  // Get an array of all pairs of adjacent letters in a string
+  let pairs = [];
+  for (let i = 0; i < s.length - 1; i++) {
+    pairs[i] = s.slice(i, i + 2);
+  }
+  return pairs;
+};
 
 // Sort array with dice Comparator
-const simSort = (s1,arr1) => {
-    // Comparison should not check case or whitespace  
-    return arr1.sort(function(a,b) {
-        let v1 = twoStrings(s1,a)
-        let v2 = twoStrings(s1,b);
-        return v2-v1 
-    });
-
+const simSort = (s1, arr1) => {
+  // Comparison should not check case or whitespace
+  return arr1.sort(function (a, b) {
+    let v1 = twoStrings(s1, a);
+    let v2 = twoStrings(s1, b);
+    return v2 - v1;
+  });
 };
 
 exports.twoStrings = twoStrings;
--- index.test.js
+++ index.test.js
@@ -1,20 +1,17 @@
-const dsc = require('./index');
-
+const dsc = require("./index");
 
 /** Comparing 2 exact same strings returns 1 */
-test('matching two equal strings', () => {
-    expect(dsc.twoStrings('hello world', 'hello world')).toBe(1);
+test("matching two equal strings", () => {
+  expect(dsc.twoStrings("hello world", "hello world")).toBe(1);
 });
-
 
 /** Comparing 2 different strings returns ]0,1[ */
-test('matching two different strings', () => {
-    expect(dsc.twoStrings('hello world', 'hello world!')).toBeGreaterThan(0.9);
-    expect(dsc.twoStrings('hello world', 'Hello es6')).toBeGreaterThan(0.4);
+test("matching two different strings", () => {
+  expect(dsc.twoStrings("hello world", "hello world!")).toBeGreaterThan(0.9);
+  expect(dsc.twoStrings("hello world", "Hello es6")).toBeGreaterThan(0.4);
 });
 
-
 /** Comparing 2 two completely different strings returns 0 */
-test('matching two different strings', () => {
-    expect(dsc.twoStrings('hello world', 'es6')).toBe(0);
+test("matching two different strings", () => {
+  expect(dsc.twoStrings("hello world", "es6")).toBe(0);
 });
